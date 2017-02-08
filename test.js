d1e2561202fe1b665b09a0516421107dad3f937d
Samin Zachariah <saminzachariah@seattleacademy.org>
2017-02-08 09:51:07 -0800

    remove flight commands

--- test.js
+++ test.js
@@ -1,42 +1,23 @@
 var arDrone = require('ar-drone');
-var client  = arDrone.createClient({ip: '192.168.0.1'});
+var client  = arDrone.createClient();
  
-//client.takeoff();
- 
-// client
-//   .after(3000, function() {
-//     this.clockwise(1);
-//   })
-//   .after(1500, function() {
-//     this.stop();
-//     this.land();
-//   });
-
 client.on("navdata",function(result){
 	console.log(result);
 })
 
-client.animateLeds('blinkGreenRed',10,2)
-
-client.takeoff();
-
-client
-	.after(1000, function(){
-	this.calibrate(0);
-	})
-	// .after(5000, function(){
-	// 	this.animate('flipLeft', 1000);
-	// })
-	.after(10000, function(){
-		this.stop();
-		this.land();
-	});
+client.animateLeds('blinkGreenRed',10,5)
 
 
-//   .after(3000, function() {
-//     this.clockwise(1);
-//   })
-//   .after(1500, function() {
-//     this.stop();
-//     this.land();
-//   });
+// client.takeoff();
+
+// client
+// 	.after(1000, function(){
+// 	this.calibrate(0);
+// 	})
+// 	// .after(5000, function(){
+// 	// 	this.animate('flipLeft', 1000);
+// 	// })
+// 	.after(10000, function(){
+// 		this.stop();
+// 		this.land();
+// 	});
